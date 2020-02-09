2cf23b5c259ac416aa5bd8d8c883b00b285ce2a4
amphioxus <armin.hinterwirth@icloud.com>
2020-02-09 10:55:37 -0800

    Added argparse stuff

--- perlin_noise_field/perlin_noise_arrows.py
+++ perlin_noise_field/perlin_noise_arrows.py
@@ -7,6 +7,7 @@ A script to produce a 2D field of flow vectors aligned using Perlin Noise
 * Can export to GCode for plotting. Parameters need to be adjusted to fit one's plotter)
 
 """
+import argparse
 import math
 import svgwrite
 import numpy as np
@@ -21,28 +22,48 @@ try:
 except ImportError:
     print('Noise module needs to be installed.\nSee: ')
     print('https://github.com/caseman/noise')
-
-
+    
+    
 #############################################################
 #%%  SET PARAMETERS HERE:
 draw_svg=True
-export_gcode=False
-octaves = 1
-lacunarity=2.0
-offset = (20,20) # where on the plotter bed should we start?
-stepsize=5
-height = 250
-width = 100
+export_gcode=True
+# octaves = 2
+# lacunarity=2.0
+#offset = (20,20) # where on the plotter bed should we start?
+# stepsize=5
+# height = 250
+# width = 250
 arrowlength=3
 tiplength=.20 # relative length of arrow tips (rel. to arrowlength). No tip if zero
 border = 2*arrowlength # border for boundary box
 
-
-# file names for export
-fn='arrow_field-f{}-lac{}_{}x{}_{}step.svg'.format(octaves, lacunarity, width, height, stepsize)
-fngc='arrow_field-f{}-lac{}_{}x{}_{}step.gcode'.format(octaves, lacunarity, width, height, stepsize)
 #############################################################
 
+
+def parse_args():
+    PARSER = argparse.ArgumentParser(description='Make a field of arrows either as an SVG image, or in gcode. Units are mm.')
+    PARSER.add_argument('width', help='Width of bed (mm)', type=int, nargs=1)
+    PARSER.add_argument('height', help='Height of bed (mm)', type=int,nargs=1)    
+    PARSER.add_argument('stepsize', help='Stepsize, i.e. distance between arrows. (mm).', type=int,nargs=1)
+    PARSER.add_argument('--offset', type=int, nargs=2, default=[50, 50],
+                        help="Offset from origin")
+    PARSER.add_argument('-s0', '--g0_speed', help='Speed in mm/min for G0 command (fast placement)',
+                        default=4000, type=int)
+    PARSER.add_argument('-s1', '--g1_speed', help='Speed in mm/min for G1 command (pen movement)',
+                        default=1500, type=int)
+    #PARSER.add_argument('-v', '--verbose', help='Print out detailed info.', action="store_true")
+    PARSER.add_argument('--pen_up', 
+                        help='GCode for Pen Up movement.', type=str, default="M3 S120\nG4 P0.5; Pen up\n")
+    PARSER.add_argument('--pen_down', 
+                        help='GCode for Pen Down movement.', type=str, default="M3 S10\nG4 P0.5; Pen down\n")
+    # octaves=1, persistence=0.5, lacunarity=2.0, repeatx=1024, repeaty=1024
+    PARSER.add_argument('-n', '--noise_params', help='Parameters for Perlin noise, separated by '\
+        'a pipe  character. Parameters: p,s{pnoise or snoise}|octaves|persistence|lacunarity|repeat x|repeat y ', default='p|2|0.5|2.0|4096|4096')
+    return PARSER.parse_args()
+    
+    
+    
 def save_gcode_to_file(gc_str, fn):
     with open(fn, 'w') as f:
         f.write(gc_str)
@@ -81,7 +102,24 @@ def get_gcode_rectangle(ll, w, h, border, pendown, penup, s0=3000, s1=1000):
     gcode+=penup
     
     return gcode
-    
+
+def parse_noise_params(s):
+    #print("noise params")
+    #print(s)
+    params = s.split('|')
+    if len(params) != 6:
+        em = 'Noise parameter must contain 6 elements, separated by |. \n'
+        em +='noise type(p or s)|octaves|persistence|lacunarity|repeatx|repeaty'
+        em +='E.g: "p|3|0.5|2|1024|1024"'
+        raise ValueError(em)
+    # cast into proper number types:
+    ntype, o, p, l, rx, ry = params
+    octaves=int(o)
+    persistence = float(p)
+    lacunarity = float(l)
+    repeatx = int(rx)
+    repeaty = int(ry)
+    return (ntype, octaves, persistence, lacunarity, repeatx, repeaty)
     
 class Arrow(object):
     """Describes an arrow with 4 points. An arrow is set up by specifying:
@@ -180,15 +218,15 @@ class Arrow(object):
             
         gcode = ''
         # first draw a->b
-        gcode += "G0 F{} X{} Y{} Z0\n".format(g0_speed, self.a[0], ay)
+        gcode += "G0 F{} X{} Y{} Z0\n".format(self.g0_speed, self.a[0], ay)
         gcode += pendown
         gcode += "G1 X{} Y{} Z0\n".format(self.b[0], by)
         gcode += penup
         if self.hastip:
             # now draw c->b->d
-            gcode += "G0 F{} X{} Y{} Z0\n".format(g0_speed, self.c[0], cy)
+            gcode += "G0 F{} X{} Y{} Z0\n".format(self.g0_speed, self.c[0], cy)
             gcode += pendown
-            gcode += "G1 F{} X{} Y{} Z0\n".format(g1_speed, self.c[0], cy)
+            gcode += "G1 F{} X{} Y{} Z0\n".format(self.g1_speed, self.c[0], cy)
             gcode += "G1 X{} Y{} Z0\n".format(self.b[0], by)
             gcode += "G1 X{} Y{} Z0\n".format(self.d[0], dy) 
             gcode += penup
@@ -196,10 +234,48 @@ class Arrow(object):
         
         return gcode
 
-def main():
+def main(config):
+    width = config.width[0]
+    height = config.height[0]
+    stepsize = config.stepsize[0]
+    offset = config.offset
+    g0_speed = config.g0_speed # mm/min for fast positioning moves
+    g1_speed = config.g1_speed # mm/min for pen motion
+    pen_down = config.pen_down
+    pen_up = config.pen_up
+    ntype, octaves, persistence, lacunarity, repeatx, repeaty = parse_noise_params(config.noise_params)
+    if ntype == 's':
+        noisetype = snoise2
+    elif ntype == 'p':
+        noisetype = pnoise2
+    else:
+        raise ValueError('Noise type must be "s" (simplex) or "p" (perlin improved noise).')
+        
     if (width % stepsize) or (height % stepsize):
         raise ValueError('Width and height need to be evenly divisible by stepsize.')
+    # file names for export
+    fn='arrow_field-f{}-lac{}_{}x{}_{}step.svg'.format(octaves, lacunarity, width, height, stepsize)
+    fngc='arrow_field-f{}-lac{}_{}x{}_{}step.gcode'.format(octaves, lacunarity, width, height, stepsize)
     
+    print('Parameters:')
+    print('Width: {}mm'.format(width))
+    print('Height: {}mm'.format(height))
+    print('Stepsize: {}mm'.format(stepsize))
+    print('Offset: {}/{}'.format( offset[0], offset[1]))
+    print('Noise params:')
+    print('\tOctaves: {}'.format(octaves))
+    print('\tPersistence: {}'.format(persistence))
+    print('\tLacunarity: {}'.format(lacunarity))
+    print('\trepeatx: {}'.format(repeatx))
+    print('\trepeaty: {}'.format(repeaty))
+    
+    if export_gcode:
+        print('GCODE params')
+        print('Pen up command: {}'.format(pen_up))
+        print('Pen down command: {}'.format(pen_down))
+        print('G0 speed: {} mm/min'.format(g0_speed))
+        print('G1 speed: {} mm/min'.format(g1_speed))
+        
     W = int(width/stepsize)+1
     H = int(height/stepsize)+1
 
@@ -209,15 +285,18 @@ def main():
     # collect angles for 2D arrow field into a list:
     for y in range(0,H):
         for x in range(0,W):
-            nraw=snoise2(x / freq,
+            nraw=noisetype(x / freq,
                          y / freq,
                          octaves,
-                         lacunarity=lacunarity)
+                         persistence = persistence,
+                         lacunarity = lacunarity,
+                         repeatx = repeatx,
+                         repeaty = repeaty)
             n = int(nraw * max_angle-1 + max_angle)
             angles.append( n )
     print('Angles: n = {}'.format(len(angles)))
     # Export as SVG file:
-
+    
     if draw_svg:
         dwg = svgwrite.Drawing(filename=fn,
                                 size=( "{}mm".format(width+2*offset[0]+border), 
@@ -244,33 +323,31 @@ def main():
     # Create gcode of arrow field if flag is set:
     if export_gcode:
         # some gcode before actual drawing happens:
-        pen_down = "M3 S10\nG4 P0.5; Pen down\n"
-        pen_up = "M3 S120\nG4 P0.5; Pen up\n"
-        g0_speed = 4000 # mm/min for fast positioning moves
-        g1_speed = 1000 # mm/min for pen motion
         gcode_string = """
-    ; GCODE Generated by Python script perlin_noise_arrows.py
-    ; Perlin noise field
-    G21; mm-mode
-    G54; Work Coordinates
-    G21; mm-mode
-    G90; Absolute Positioning
-    ; Plotter Mode Active
-    M3 S120; pen up
-    G0 Z0
-    G0 F{s0} X{x} Y{y}; go to {x}/{y}
-    G1 F{s1}; speed when plotting
+; GCODE Generated by Python script perlin_noise_arrows.py
+; Perlin noise field
+G21; mm-mode
+G54; Work Coordinates
+G21; mm-mode
+G90; Absolute Positioning
+; Plotter Mode Active
+M3 S120; pen up
+G0 Z0
+G0 F{s0} X{x} Y{y}; go to {x}/{y}
+G1 F{s1}; speed when plotting
 
     """.format( x=offset[0], 
                 y=offset[1],
                 s1=g1_speed,
                 s0=g0_speed)
-        draw_arrows=True # debug
+        draw_arrows=False # debug
         if draw_arrows:
             # Now draw the arrow objects:
             c=0
-            for y in range(offset[1],height+2*offset[1], stepsize):
-                for x in range(offset[0],width+2*offset[0], stepsize):
+            for yt in range(0, H):
+                for xt in range(0, W):
+                    x = xt*stepsize + offset[0]
+                    y = yt*stepsize + offset[1]
                     a = Arrow((x,y), 
                               length=arrowlength, 
                               dir=angles[c], 
@@ -289,8 +366,7 @@ def main():
                                                  height+2*offset[1],
                                                  border,
                                                  pen_down,
-                                                 pen_up)
-        
+                                                 pen_up)        
         # go back home with pen raised
         gcode_string += "G0 F{} X0 Y0; go to 0/0".format(g0_speed)
     
@@ -299,6 +375,7 @@ def main():
     
 
 if __name__=="__main__":
-    main()
+    config = parse_args()
+    main(config)
     
     
